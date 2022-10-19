8eea0d3da889b9ee9fc381f19f4f7514db791156
h-mayorquin <h.mayorquin@gmail.com>
2022-10-19 18:02:00 +0200

    fix write to processing

--- src/murthy_lab_to_nwb/li2022ecephys/li2022ecephysinterface.py
+++ src/murthy_lab_to_nwb/li2022ecephys/li2022ecephysinterface.py
@@ -5,6 +5,8 @@ import h5py
 from spikeinterface.extractors import NumpyRecording
 
 from neuroconv.datainterfaces.ecephys.baserecordingextractorinterface import BaseRecordingExtractorInterface
+from neuroconv.utils import get_schema_from_hdmf_class
+from pynwb.ecephys import ElectricalSeries
 
 
 class Li2022EcephysInterface(BaseRecordingExtractorInterface):
@@ -13,15 +15,15 @@ class Li2022EcephysInterface(BaseRecordingExtractorInterface):
 
     def __init__(self, file_path: str, verbose: bool = True):
 
-        file = h5py.File(file_path, "r")
-        header = file["header"]
+        self.file = h5py.File(file_path, "r")
+        self.header = self.file["header"]
 
-        traces_name_list = sorted([key for key in file.keys() if "sweep" in key])
+        traces_name_list = sorted([key for key in self.file.keys() if "sweep" in key])
 
-        channel_ids = header["AIChannelNames"][()]
-        traces_list = [file[trace_name]["analogScans"][()].T for trace_name in traces_name_list]
-        t_starts = [file[trace_name]["timestamp"][()] for trace_name in traces_name_list]
-        sampling_frequency = file["header"]["AcquisitionSampleRate"][()]
+        channel_ids = self.header["AIChannelNames"][()]
+        traces_list = [self.file[trace_name]["analogScans"][()].T for trace_name in traces_name_list]
+        t_starts = [self.file[trace_name]["timestamp"][()] for trace_name in traces_name_list]
+        sampling_frequency = self.header["AcquisitionSampleRate"][0, 0]
 
         super().__init__(
             verbose=verbose,
@@ -32,12 +34,29 @@ class Li2022EcephysInterface(BaseRecordingExtractorInterface):
         )
         self.recording_extractor
 
-        channel_units = header["AIChannelUnits"][()]
-        gains = 1.0 / header["AIChannelScales"][()]
+        channel_units = self.header["AIChannelUnits"][()]
+        gains = 1.0 / self.header["AIChannelScales"][()].flatten()
         self.recording_extractor.set_channel_gains(gains)
         self.recording_extractor.set_channel_offsets(offsets=0)
         self.recording_extractor.set_property(key="channel_units", values=channel_units.astype("str"))
 
+    # def get_metadata_schema(self):
+    #     metadata_schema = super().get_metadata_schema()
+
+    #     metadata_schema["properties"]["Ecephys"]["properties"].update(
+    #         ElectricalSeriesProcessed=get_schema_from_hdmf_class(ElectricalSeries)
+    #     )
+    #     return metadata_schema
+
+    # def get_metadata(self):
+    #     metadata = super().get_metadata()
+
+    #     metadata["Ecephys"]["ElectricalSeriesProcessed"] = dict(
+    #         name="ElectricalSeriesProcessed", description="Processed data from wavesurfer"
+    #     )
+
+    #     return metadata
+
     def get_conversion_options(self):
-        conversion_options = dict(write_as="processed", es_key="ElectricalSeriesProcessed", stub_test=False)
+        conversion_options = dict(write_as="processed", stub_test=False)
         return conversion_options
